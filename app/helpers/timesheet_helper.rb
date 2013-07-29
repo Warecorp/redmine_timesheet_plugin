c47112a02d4611b9a3439c4d4ab8cd69edff14f9
Alex Klim <alex.klimenkov89@gmail.com>
2013-07-29 15:31:15 +0300

    change plugin for rails 3.2

--- app/helpers/timesheet_helper.rb
+++ app/helpers/timesheet_helper.rb
@@ -1,4 +1,6 @@
 module TimesheetHelper
+  include ProjectsHelper
+
   def showing_users(users)
     l(:timesheet_showing_users) + users.collect(&:name).join(', ')
   end
@@ -34,7 +36,7 @@ module TimesheetHelper
     style ||= ''
 
     content_tag(:span,
-                link_to_function(image_tag(image, :plugin => "timesheet_plugin"), js),
+                link_to_function(image_tag(image, :plugin => "redmine_timesheet_plugin"), js),
                 :class => "toggle-" + issue_id.to_s,
                 :style => style
                 )
@@ -50,10 +52,7 @@ module TimesheetHelper
     selected_projects = timesheet.projects.collect(&:id)
     selected_projects = available_projects.collect(&:id) if selected_projects.blank?
     
-    options_from_collection_for_select(available_projects,
-                                       :id,
-                                       :name,
-                                       selected_projects)
+    project_tree_options_for_select(available_projects, :selected => selected_projects)
   end
 
   def activity_options(timesheet, activities)
