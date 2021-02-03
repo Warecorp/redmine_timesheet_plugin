d6a48214518761185377e6d95b1aa3e5be4f914b
Sergey Yelenski <sergey.yelenski@warecorp.com>
2021-02-03 11:06:19 +0300

    Migration changed

--- db/migrate/002_create_enum_prices.rb
+++ db/migrate/002_create_enum_prices.rb
@@ -1,9 +1,10 @@
 class CreateEnumPrices < ActiveRecord::Migration[5.2]
   def change
-    create_table :prices true do |t|
+    create_table :prices do |t|
       t.references :enumerations
       t.float :price
     end
-    create_index :prices, :enumerations_id
+    remove_index :prices, :enumerations_id
+    add_index :prices, :enumerations_id
   end
 end
